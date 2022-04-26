fb80f081f890736577dce72da0b319331f99a2ad
Abel Abraham <abelaj50@gmail.com>
2022-04-26 00:42:42 -0500

    Added icons for BottomTabNavigator

--- App.js
+++ App.js
@@ -9,16 +9,34 @@ import Portfolio from "./screens/Portfolio";
 import Settings from "./screens/Settings";
 import "./styles/styles.js";
 import styles from "./styles/styles";
+import TabBarIcon from '@react-navigation/bottom-tabs/src/views/TabBarIcon';
+import { Image } from "react-native";
 
+/* ICONS used fairly as part of the Flaticon License: "Free for personal and commercial purpose with attribution". Credits go to users Freepik, Miroliubov, and Gregor Cresnar Premium. */
 
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
     <Tab.Navigator backBehavior='history'>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Portfolio" component={Portfolio} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen name="Home" component={HomeScreen}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({focused}) => (
+          <Image source={require("./assets/icons/home.png")} style={{width: 25, height: 25, tintColor: focused ? '#1c8cfc' : '#a4a4a5'}} />)
+        }} />
+      <Tab.Screen name="Portfolio" component={Portfolio}
+        options={{
+          title: 'Portfolio',
+          tabBarIcon: ({focused}) => (
+          <Image source={require("./assets/icons/portfolio.png")} style={{width: 25, height: 25, tintColor: focused ? '#1c8cfc' : '#a4a4a5'}} />)
+        }} />
+      <Tab.Screen name="Settings" component={Settings}
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({focused}) => (
+          <Image source={require("./assets/icons/settings.png")} style={{width: 25, height: 25, tintColor: focused ? '#1c8cfc' : '#a4a4a5'}} />)
+        }} />
     </Tab.Navigator>
   );
 }

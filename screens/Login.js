63444cd508c48df5506287e5659d3641a65da4ee
dvilla55 <dvilla55@uic.edu>
2022-04-26 20:13:18 -0500

    Addition of Login screen

---
+++ screens/Login.js
@@ -0,0 +1,26 @@
+import {ImageBackground, Pressable, Text, View} from "react-native";
+import {StatusBar} from "expo-status-bar";
+import React, {Component} from "react";
+import styles from "../styles/styles";
+import {ScrollView} from "react-native";
+
+class Login extends Component {
+    render() {
+        return (
+            <View style={styles.backdrop}>
+                <ImageBackground source={require('../assets/bgimage.png')} style={styles.image}>
+                    <Text style={styles.logo}>StockUp</Text>
+                    <Button  
+                    title="Go to Profile"  
+                    onPress={() => this.props.navigation.navigate('Home')}  
+                    />  
+                </ImageBackground>
+
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
+}
+
+export default Login;
+

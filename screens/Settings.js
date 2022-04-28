5830fec07fde2fff0af3d6c5e966356eb8a79ffe
moody <samkary2@uic.edu>
2022-04-27 21:33:29 -0500

    Added functionality to the settings page

--- screens/Settings.js
+++ screens/Settings.js
@@ -3,7 +3,7 @@ import styles from "../styles/styles";
 import {StatusBar} from "expo-status-bar";
 import React, {Component} from "react";
 import { Button } from "react-native";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Modal, TextInput } from "react-native";
 import { useState } from "react";
 import {Pressable} from "react-native";
 import { Switch } from "react-native";
@@ -15,8 +15,86 @@ function Settings(){
     const [switch1,setSwitch1]=useState(false)
     const [switch2,setSwitch2]=useState(false)
     const [switch3,setSwitch3]=useState(false)
+    const [modal1,setModal1]=useState(false)
+    const [modal2,setModal2]=useState(false)
+    const [modal3,setModal3]=useState(false)
+    const [modal4,setModal4]=useState(false)
     return (
         <View style={styles.backdrop}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modal1}
+                onRequestClose={() => {
+                    setModal1(!modal1);
+                }}
+            >
+                <View style={styles.modalView}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="enter your email"
+                        keyboardType="default"
+                    />
+                    <Pressable style={styles.button} onPress={()=>setModal1(!modal1)}>
+                        <Text>Confirm</Text>
+                    </Pressable>
+                </View>
+            </Modal>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modal2}
+                onRequestClose={() => {
+                    setModal2(!modal2);
+                }}
+            >
+                <View style={styles.modalView}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="enter your password"
+                        keyboardType="default"
+                    />
+                    <Pressable style={styles.button} onPress={()=>setModal2(!modal2)}>
+                        <Text>Confirm</Text>
+                    </Pressable>
+                </View>
+            </Modal>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modal3}
+                onRequestClose={() => {
+                    setModal3(!modal3);
+                }}
+            >
+                <View style={styles.modalView}>
+                    <Text>Are you sure you want to enable biometric sign-in</Text>
+                    <Pressable style={styles.button} onPress={()=>setModal3(!modal3)}>
+                        <Text>Yes</Text>
+                    </Pressable>
+                    <Pressable style={styles.button} onPress={()=>setModal3(!modal3)}>
+                        <Text>No</Text>
+                    </Pressable>
+                </View>
+            </Modal>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modal4}
+                onRequestClose={() => {
+                    setModal4(!modal4);
+                }}
+            >
+                <View style={styles.modalView}>
+                    <Text>Are you sure you want to enable 2-factor Authentication</Text>
+                    <Pressable style={styles.button} onPress={()=>setModal4(!modal4)}>
+                        <Text>Yes</Text>
+                    </Pressable>
+                    <Pressable style={styles.button} onPress={()=>setModal4(!modal4)}>
+                        <Text>No</Text>
+                    </Pressable>
+                </View>
+            </Modal>
             <ImageBackground source={require('../assets/bgimage.png')} style={styles.image}>
                 <View style={styles.container}>
                     <Text style={styles.titleText}>JustinThyme1</Text>
@@ -30,10 +108,10 @@ function Settings(){
                         </View>
                         {upClicked &&
                             <View style={styles.container}>
-                                <Pressable style={styles.button}>
+                                <Pressable style={styles.button} onPress={()=>setModal1(!modal1)}>
                                     <Text>Change Email</Text>
                                 </Pressable>
-                                <Pressable style={styles.button}>
+                                <Pressable style={styles.button} onPress={()=>setModal2(!modal2)}>
                                     <Text>Change Password</Text>
                                 </Pressable>
                             </View>
@@ -81,10 +159,10 @@ function Settings(){
                         </View>
                         {security &&
                             <View style={styles.container}>
-                                <Pressable style={styles.button}>
+                                <Pressable style={styles.button} onPress={()=>setModal3(!modal3)}>
                                     <Text>Enable Biometric Sign-in</Text>
                                 </Pressable>
-                                <Pressable style={styles.button}>
+                                <Pressable style={styles.button} onPress={()=>setModal4(!modal4)}>
                                     <Text>Enable 2-Factor Authentication</Text>
                                 </Pressable>
                             </View>
--- styles/styles.js
+++ styles/styles.js
@@ -14,6 +14,21 @@ const styles = StyleSheet.create({
         width: '100%',
         position: 'absolute'
     },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
     icon: {
         margin: 5
     },
